767088e36171889d25db757cd01a53a929360923
soeltan zaki rizaldy <zcky1990@gmail.com>
2024-06-16 16:41:56 +0700

    add function envelope

--- static/javascript/components/Envelope.vue
+++ static/javascript/components/Envelope.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid content-center h-screen gap-2 px-4 bg-white">
+    <div v-if="show" class="grid content-center h-screen gap-2 px-4 bg-white">
         <div class="flex flex-col justify-center gap-4 mb-10 text-center">
             <p class="text-3xl tracking-widest text-gray-500 uppercase">The wedding of</p>
             <p class="text-2xl tracking-widest text-gray-500 uppercase">Bride</p>
@@ -14,7 +14,8 @@
             </div>
 
             <div class="flex flex-row justify-center">
-                <a class="inline-block w-2/4 px-8 py-3 text-sm font-medium text-indigo-600 transition border border-current rounded-full hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-indigo-500"
+                <a @click="closeEnvelope"
+                    class="inline-block w-2/4 px-8 py-3 text-sm font-medium text-indigo-600 transition border border-current rounded-full hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-indigo-500"
                     href="#">
                     Open Invitation
                 </a>
@@ -24,6 +25,15 @@
 </template>
 <script>
 export default {
-
+    data() {
+        return {
+            show: true,
+        }
+    },
+    methods:{
+        closeEnvelope(){
+            this.show = false;
+        }
+    }
 }
 </script>
